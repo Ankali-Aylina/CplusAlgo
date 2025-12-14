4703d15f8275d7df6bca8de42c65c879192b984a
Ankali-Aylina <ankali_aylina@qq.com>
2025-12-14 23:25:16 +0800

    添加了贪心算法

--- 003-Greedy/readme.md
+++ 003-Greedy/readme.md
@@ -2,6 +2,32 @@
 
 ## 简介
 
+- 贪心算法示例，添加了最小堆操作。
+
 ## 需求
 
-## 练习
+### 题目描述：有n个人排队到r个水龙头前去打水，他们装满水桶的时间分别为t1,t2,t3,…,tn，打水时间均为整数且各不相等，应该如何安排他们的打水顺序才能使他们花费的总时间最少？每个人的打水时间=排队时间+装满水桶的时间(假设排队切换等其他过程均不消耗时间)
+
+### 要求程序
+
+- 输入：有两行，第1行为两个整数，分别是n和r,即人数和水龙头数。
+(1<=n<=500)(1<=r<=100)
+- 第二行为n个正整数t1,t2,t3,…,ti(1<=ti<=1000)。表示每个人装满水桶的时间
+- 输出：1行，一个正整数，表示他们花费的最少总时间。
+
+## 代码解释
+
+```c++
+/// 使用&引用
+void getWater(int per, int num, std::vector<int> &time)
+
+ std::priority_queue<int, std::vector<int>, std::greater<int>>
+ // std::priority_queue<队列元素类型, 底层容器类型, 比较函数类型>
+
+ // 默认情况：最大堆（不加第三个参数）
+// std::priority_queue<int> max_heap;
+// 等价于：
+// std::priority_queue<int, std::vector<int>, std::less<int>> max_heap;
+// std::less<int>：元素越大，优先级越高,堆顶是最大值
+// std::gregreater<int>: 元素越小，优先级越高,堆顶是最小值
+```
--- 003-Greedy/src/main.cpp
+++ 003-Greedy/src/main.cpp
@@ -1,6 +1,59 @@
 #include "main.h"
+#include <algorithm>
+#include <vector>
+#include <queue>
 
+/**
+ * 计算最少时间
+ * @param per 人数
+ * @param num 水龙头数
+ * @param time 每个人打水时间
+ */
+void getWater(int per, int num, std::vector<int> &time)
+{
+    // 排序，将最少的时间排在前
+    std::sort(time.begin(), time.end());
+
+    // 创建最小堆用于用于寻找最早空闲的水龙头
+    std::priority_queue<int, std::vector<int>, std::greater<int>> water_taps;
+
+    // 先将水龙头时间都调整为0
+    for (int i = 0; i < num; i++)
+    {
+        water_taps.push(0);
+    }
+    // 总时间计数
+    int total_time = 0;
+
+    for (int i = 0; i < per; i++)
+    {
+        // 读取堆顶（最早空闲的水龙头）时间
+        int earliest = water_taps.top();
+        water_taps.pop(); // 弹出堆顶
+
+        // 这个人花费的时间 = 等待时间 + 打水时间
+        int per_time = earliest + time[i];
+
+        // 累加总时间
+        total_time += per_time;
+
+        // 更新水龙头时间（下一个使用水龙头的人时间在前一个人的时间上累加）
+        water_taps.push(per_time);
+    }
+    std::cout << "总时间为：" << total_time << std::endl;
+}
 int main()
 {
+    int r, n, s = 0;
+    std::cout << "请输入人数和水龙头数：" << std::endl;
+    std::cin >> n >> r;
+    std::vector<int> data(n);
+    std::cout << "请输入" << n << "个人的打水时间：" << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> data[i];
+    }
+    getWater(n, r, data);
+
     return 0;
 }
